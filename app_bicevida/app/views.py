f68bdfa9f3b9759ee87e57159e660560732a99d0
gucciknight <arodriguezl3@correo.uss.cl>
2024-06-20 19:22:03 -0400

    rededefinimos url de app para la url general

--- app_bicevida/app/views.py
+++ app_bicevida/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+import requests
 
-# Create your views here.
+#obtenemos los datos entregados en el endpoint con la función get
+class AppView(APIView):
+    def get(self, request):
+        response = requests.get('https://dn8mlk7hdujby.cloudfront.net/interview/insurance/58')
+        return Response(response.json())
+
