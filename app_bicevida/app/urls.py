84e6ece619963d042373ac862fd2f1170db160d1
gucciknight <arodriguezl3@correo.uss.cl>
2024-06-20 22:38:28 -0400

    Se ha creado la función get que renderiza los datos del endpoint en conjunto con su url

--- app_bicevida/app/urls.py
+++ app_bicevida/app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import AppView
 
 urlpatterns = [
-    path('', AppView.as_view(), name='app'),
+    path('', AppView.as_view(), name='home'),
 ]
 
--- app_bicevida/app/views.py
+++ app_bicevida/app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.http import JsonResponse
+from django.views import View
 import requests
 
-#obtenemos los datos entregados en el endpoint con la función get
-class AppView(APIView):
+class AppView(View):
     def get(self, request):
-        response = requests.get('https://dn8mlk7hdujby.cloudfront.net/interview/insurance/58')
-        return Response(response.json())
+        url = "https://dn8mlk7hdujby.cloudfront.net/interview/insurance/58"
+        response = requests.get(url)
+        data = response.json()
 
+        insurance = data.get('insurance', {})
+
+        return render(request, 'app_interface.html', {'insurance': insurance})
